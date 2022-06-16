124fcaa86d9f2bfd645649fbd8c5fe02e26e05c2
MIN SIK CHOI <minstones34@gmail.com>
2022-06-17 03:18:04 +0900

    Changing oolor & brush size

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
-canvas.width = 500;
-canvas.height = 500;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 600;
 
-ctx.strokeStyle = "#2c2c2c";
-ctx.lineWidth = 2.5;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 2.5; //브러쉬 굵기 초기값
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -31,18 +38,51 @@ function onMouseMove(event) {
 }
 
 function handleColorClick(event) {
-  const color = event.target.style.backgroundColor;
+  const color = event.target.style.backgroundColor; //console.log(event.target.style)에서 관련 요소인 backgroundColor를 찾음
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+//먼저 console.log(event)로 이벤트 정보를 확인, target 란을 펼쳐서 원하는 요소인 value를 확인
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
 }
 
 if (canvas) {
+  //if 구문으로 조건을 확실히 해주는 습관을 들이자
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 );
-//array.from은 해당 오브젝트로부터 배열을 만들어줌.
+//array.from은 해당 오브젝트로부터 배열을 만들어줌. 이과정을 생략하면 console.log(colors)의 결과가 html 컬렉션으로 나옴.
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+} //html에 input에 따라 조정되야 하기 때문에
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
